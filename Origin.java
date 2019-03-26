ce4514af233803e9e1b8f592e050adc985ac78cc
imdev96 <32494590+imdev96@users.noreply.github.com>
2019-03-26 20:53:04 +0200

    Add files via upload

---
+++ Origin.java
@@ -0,0 +1,25 @@
+class Origin
+{
+	private String text;
+	private ArrayList<String> answers;
+	
+	public void setText(String t)
+	{
+		text = t;
+	}
+	
+	public void setAnswers(ArrayList<String> a)
+	{
+		answers = a;
+	}
+	
+	public String getText()
+	{
+		return text;
+	}
+	
+	public ArrayList<String> getAnswers()
+	{
+		return answers;
+	}
+}
